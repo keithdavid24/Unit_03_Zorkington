d9993410abe96aa897b71453eb77e1f02d458d4a
keithdavid24 <keithdavid247@gmail.com>
2023-09-03 14:36:30 -0600

    Removed the code for drop item that needed review from connor

--- script.js
+++ script.js
@@ -254,16 +254,6 @@ export const domDisplay = (playerInput) => {
         currentRoom.items.splice(currentRoom.items.indexOf(argument), 1)
         player.inventory.push(argument)
         return `You picked up the ${argument}.`
-        // } else if (command === 'drop') {
-        //     if (player.inventory.includes(argument)) {
-        //         const currentRoom = roomState[argument]
-        //         player.drop(argument)
-        //         currentRoom.dropItem(argument)
-        //         return 'You dropped the ${argument}';
-        //     } else {
-        //         return `You don't have ${argument} in your inventory.`
-        //     }
-        // }
     } else if (command === 'drop' && player.inventory.includes(argument)) {
         currentRoom.items.push(argument)
         player.inventory.splice(player.inventory.indexOf(argument), 1)
